a5739a5eba420429d0bf6a4660c6bf9a927b8506
John Lloyd Patrick Japos <dyollsopaj@gmail.com>
2021-07-12 23:48:02 +0800

    added web icon

--- public/partials/head.php
+++ public/partials/head.php
@@ -3,5 +3,6 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Vending Machine</title>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+	<link rel="icon" href="img/icon.svg">
 	<link rel='stylesheet' type='text/css' href='css/styles2.css'>
 </head>
